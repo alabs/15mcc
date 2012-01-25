1cbbca63ab022ced9b75db6aaa46365d0c58be5b
Antonio Pardo <apardo@alabs.es>
2012-01-25 13:56:37 +0100

    prototyping images

--- webapp/app/controllers/images_controller.rb
+++ webapp/app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   # GET /images
   # GET /images.json
   def index
-    @images = Image.all
+    @images = Image.all.desc(:created_at)
 
     respond_to do |format|
       format.html # index.html.erb
