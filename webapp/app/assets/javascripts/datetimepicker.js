75a68a2c13e010073d16941b2d910dcb5ba69daf
Andrés Pereira <apereira@alabs.es>
2012-02-02 12:53:48 +0100

    unificando los JS del formulario

--- webapp/app/assets/javascripts/datetimepicker.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  $('#text_happened_at').datetimepicker({
-    dateFormat: 'yy-mm-dd',
-    timeFormat: 'hh:mm:ss'
-  });
-
-
-})
--- webapp/app/assets/javascripts/forms.js
+++ webapp/app/assets/javascripts/forms.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  // date and time picker for Fecha
+  $('#text_happened_at').datetimepicker({
+    dateFormat: 'yy-mm-dd',
+    timeFormat: 'hh:mm:ss'
+  });
+
+  // autocomplete for tags
   $('#text_tags, #video_tags, #image_tags').tagit({ 
     tagSource: "/tags/search.json",
     removeConfirmation: true,
