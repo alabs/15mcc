6caa5291ccc1dc9da7cda54ba53e26666393bb15
Andrés Pereira <apereira@alabs.es>
2012-02-02 08:49:17 +0100

    gmaps4rails gem

--- webapp/vendor/assets/javascripts/external.js
+++ webapp/vendor/assets/javascripts/external.js
@@ -1,2 +1,9 @@
 //= require jquery-ui-1.8.17.custom.min
 //= require jquery.tagit
+//
+// gmaps4rails
+//= require gmaps4rails/bing.js
+//= require gmaps4rails/googlemaps.js
+//= require gmaps4rails/mapquest.js
+//= require gmaps4rails/openlayers.js
+//= require gmaps4rails/all_apis.js
