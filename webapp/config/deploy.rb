a282ba0bb7ecdc5b310db7ca01fbc488ade778d9
Antonio Pardo <apardo@alabs.es>
2012-02-13 12:12:32 +0100

    a ver si esta funciona

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -25,6 +25,7 @@ set :use_sudo, false
 set :scm, :git
 set :repository, "git://github.com/alabs/15mcc.git"
 set :branch, "master"
+set :deploy_subdir, "15mcc/webappr"
 set :git_enable_submodules, 1
 set :keep_releases, 5
 
