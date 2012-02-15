d212937934352e435ba5e547a9fa0f34955025ba
Andrés Pereira <apereira@alabs.es>
2012-02-15 11:43:29 +0100

    traducido y maquetado el registro de usuarios

--- webapp/config/application.rb
+++ webapp/config/application.rb
@@ -31,18 +31,10 @@ module Webapp
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Configure the default encoding used in templates for Ruby 1.9.
+    config.time_zone = 'Madrid'
+    config.i18n.default_locale = :es
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
