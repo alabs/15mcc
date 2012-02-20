b08dde5be657f6a91cfb9b4ff286e4d4128195f8
Antonio Pardo <apardo@alabs.es>
2012-02-20 13:01:41 +0100

    proto profile page

--- webapp/app/controllers/pages_controller.rb
+++ webapp/app/controllers/pages_controller.rb
@@ -5,4 +5,15 @@ class PagesController < ApplicationController
 
   def bank
   end
+
+  # método para mostrar el contenido generardo por
+  # el usuario
+  def profile
+    @user = User.where(:username => params[:username]).first
+    
+    unless @user
+      render :text => "404 Not Found", :status => 404
+      return
+    end
+  end
 end
