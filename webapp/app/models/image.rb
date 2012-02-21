dc053a36225a28ddbcccb414602881c9ac6c1c1a
pedro <pedrogonzalezj@gmail.com>
2012-02-21 12:44:46 +0100

    exif info from images

--- webapp/app/models/image.rb
+++ webapp/app/models/image.rb
@@ -17,6 +17,8 @@ class Image
       :small => ['260x180', :jpg]
     }
 
+  after_img_post_process  :process_metadata
+
   # gmaps4rails https://github.com/apneadiving/Google-Maps-for-Rails
   acts_as_gmappable :lat => 'latitude', :lon => 'longitude'
 
@@ -37,6 +39,19 @@ class Image
   def gmaps4rails_infowindow
     # TODO: extend this - put more information
     "<h1>#{ title }</h1>"
+  end
+
+  def process_metadata
+
+    exif_info = EXIFR::JPEG.new(img.queued_for_write[:original].path)
+    logger.debug "Exif INFO #{exif_info}"
+    return unless exif_info.exif?
+    #Save exif metadata
+    self.latitude  = exif_info.gps.latitude
+    logger.debug "Latitude #{self.latitude}"
+    self.longitude = exif_info.gps.longitude
+    logger.debug "Longitude #{self.longitude}"
+
   end
 
 end
