11beee9717273137333f7be2aa1d664359d1e665
Antonio Pardo <apardo@alabs.es>
2012-02-21 09:53:28 +0100

    método para incluir tipo de recurso

--- webapp/app/controllers/timeline_controller.rb
+++ webapp/app/controllers/timeline_controller.rb
@@ -2,7 +2,8 @@ class TimelineController < ApplicationController
 
   # GET /timeline
   def index
-    @contents = Text.all || Image.all || Video.all || Audio.all
+    @contents = Text.all | Image.all | Video.all | Audio.all
+    @contents.map {|c| c.write_attribute(:klass, c.class.to_s) }
 
     respond_to do |format|
       format.html # index.html.erb
