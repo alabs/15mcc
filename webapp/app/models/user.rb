5d2a2fc815e44ddf7baa60a7239e2096a2b5bed0
Antonio Pardo <apardo@alabs.es>
2012-03-01 15:32:38 +0100

    user role bug fixed

--- webapp/app/models/user.rb
+++ webapp/app/models/user.rb
@@ -64,7 +64,8 @@ class User
   ROLES = %w[admin editor user anonymous]
   
   def role?(base_role)
-    ROLES.index(base_role.to_s) <= ROLES.index(role)
+    #ROLES.index(base_role.to_s) <= ROLES.index(role)
+    role.present? && ROLES.index(base_role.to_s) <= ROLES.index(role)
   end
   
   #def role?(role)
