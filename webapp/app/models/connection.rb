d8d44b16eb182d1ac29ee789cedd2ad81d1a478e
Andrés Pereira <apereira@alabs.es>
2012-03-05 09:27:08 +0100

    #44 bug al tener conexiones duplicadas

--- webapp/app/models/connection.rb
+++ webapp/app/models/connection.rb
@@ -3,6 +3,9 @@ class Connection
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  # evitamos que haya conexiones duplicadas con mismo origen y destino
+  validates :target_id, :uniqueness => {:scope => :source_id}
+
   field :label, :type => String
   field :source_id, :type => String
   field :target_id, :type => String
