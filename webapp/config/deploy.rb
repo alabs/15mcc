ae3dffa00e50e72b1df6fb4479240f7133f83f3e
Antonio Pardo <apardo@alabs.es>
2012-03-13 14:09:29 +0100

    deploy update

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -57,10 +57,9 @@ after "deploy:update_code", "deploy:config_symlink"
 namespace :deploy do
 
   task :config_symlink do
-    run <<-CMD
-      ln -sf #{shared_path}/recaptcha.rb #{latest_release}/config/initializers/recaptcha.rb &&
-      ln -sf #{shared_path}/app_config.yml #{latest_release}/config/app_config.yml"
-    CMD
+    #run "ln -s #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
+    run "ln -sf #{shared_path}/recaptcha.rb #{release_path}/config/initializers/recaptcha.rb"
+    #run "ln -sf #{shared_path}/app_config.yml #{release_path}/config/app_config.yml"
   end
 end
 
