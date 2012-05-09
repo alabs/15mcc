82ad427918269f82dfdf842a3ff69e735130b8bb
Andrés Pereira <apereira@alabs.es>
2012-05-09 14:56:06 +0200

    quitando dependencias del mindmap

--- webapp/vendor/assets/javascripts/external.js
+++ webapp/vendor/assets/javascripts/external.js
@@ -18,14 +18,14 @@
 //= require bootstrap-dropdown
 
 // for mind-map
-//= require jsplumb/jsBezier-0.3
-//= require jsplumb/jsPlumb-1.3.6-RC1
-//= require jsplumb/jsPlumb-defaults-1.3.6-RC1
-//= require jsplumb/jsPlumb-connectors-statemachine-1.3.6-RC1
-//= require jsplumb/jsPlumb-renderers-svg-1.3.6-RC1
-//= require jsplumb/jquery.jsPlumb-1.3.6-RC1
-//= require underscore
-//= require jquery.overscroll
+// require jsplumb/jsBezier-0.3
+// require jsplumb/jsPlumb-1.3.6-RC1
+// require jsplumb/jsPlumb-defaults-1.3.6-RC1
+// require jsplumb/jsPlumb-connectors-statemachine-1.3.6-RC1
+// require jsplumb/jsPlumb-renderers-svg-1.3.6-RC1
+// require jsplumb/jquery.jsPlumb-1.3.6-RC1
+// require underscore
+// require jquery.overscroll
 
 //= require Markdown.Converter
 //= require Markdown.Editor
