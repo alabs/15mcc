3332d8b926e1af18bd07e3bb97168f529801e964
Antonio Pardo <apardo@alabs.es>
2012-05-16 16:20:21 +0200

    cookies

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def flash_notice_cookie(msg, expiration=nil)
+    flash.each do |name, msg|
+      cookies[:flash_notice] = {
+        :value => msg,
+        :expires => Time.now - 24.hours
+      }
+      cookies[:flash_notice_class] = {
+        :value => name,
+        :expires => Time.now - 24.hours
+      }
+    end
+  end
 end
