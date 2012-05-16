19046f8c395103e3920fca657c7248ff2b93f807
Antonio Pardo <apardo@alabs.es>
2012-05-16 12:30:54 +0200

    cookie added when login with facebook

--- webapp/app/controllers/users/omniauth_callbacks_controller.rb
+++ webapp/app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
 
     if @user.persisted?
+      cookies[:username] = {
+        :value => @user.username,
+        :expires => 2.days.from_now,
+        :domain => "bancodeideas.15m.cc"
+      }
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => 'Facebook'
       sign_in_and_redirect @user, :event => :authentication
     else
@@ -13,18 +18,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def google
-    #logger.info('DEBUG 15M.CC: ' + request.env['omniauth'].inspect)
-    omniauth = request.env['omniauth.auth']
-    raise omniauth.to_yaml
-    @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
+  #def google
+  #  #logger.info('DEBUG 15M.CC: ' + request.env['omniauth'].inspect)
+  #  omniauth = request.env['omniauth.auth']
+  #  raise omniauth.to_yaml
+  #  @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
+  #  if @user.persisted?
+  #    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+  #    sign_in_and_redirect @user, :event => :authentication
+  #  else
+  #    session["devise.google_data"] = request.env["omniauth.auth"]
+  #    redirect_to new_user_registration_url
+  #  end
+  #end
 end
