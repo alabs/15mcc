ac12df30f8c3f839856f5314aa9b5e0f8592d1ba
Antonio Pardo <apardo@alabs.es>
2012-05-16 14:01:41 +0200

    js handler

--- webapp/app/controllers/my_sessions_controller.rb
+++ webapp/app/controllers/my_sessions_controller.rb
@@ -8,16 +8,17 @@ class MySessionsController < Devise::SessionsController
     if current_user
       cookies[:username] = {
         :value => current_user.username,
-        :expires => 2.days.from_now,
-        :domain => "bancodeideas.15m.cc"
+        :expires => 2.days.from_now
       }
       if current_user.role == 'admin'
         cookies[:a] = {
           :value => 1,
-          :expires => 2.days.from_now,
-          :domain => "bancodeideas.15m.cc"
+          :expires => 2.days.from_now
         }
       end
+    else
+      cookies.delete(:username)
+      cookies.delete(:a)
     end
   end
 end
