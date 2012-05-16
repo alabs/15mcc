e7e5ea5c7abfd079620bb02ed1ebc42a962479d1
Antonio Pardo <apardo@alabs.es>
2012-05-16 16:51:35 +0200

    proto cache home

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
     flash.each do |name, msg|
       cookies[:flash_notice] = {
         :value => msg,
-        :expires => Time.now - 24.hours
+        :expires => expiration || 1.minutes.from_now
       }
       cookies[:flash_notice_class] = {
         :value => name,
-        :expires => Time.now - 24.hours
+        :expires => expiration || 1.minutes.from_now
       }
     end
   end
