b4827ec5c4e5546a0f0ac7a7c6c00d62e52b1a0e
Andrés Pereira <apereira@alabs.es>
2012-05-29 08:51:28 +0200

    cookies: cambiando secret token

--- webapp/config/initializers/secret_token.rb
+++ webapp/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Webapp::Application.config.secret_token = '4f7a865360235f84e46ae6e047a9d1244761139d949d8b166c780d976367e00dd12bd4809d43aba8dd21088607f44421fb64a6aeb049011ded4c7206a82976cf'
+Webapp::Application.config.secret_token = '4f7a865360235f84e46ae6e047a9d1244761139d949d8b166c780d976367e00dd12bd4809d43aba8dd21088607f44421fb64a6aeb049011ded4c7206a82976df'
