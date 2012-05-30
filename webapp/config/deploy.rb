78037708e017d5575b40114e047220235649eeff
Antonio Pardo <apardo@alabs.es>
2012-05-30 16:27:19 +0200

    probando deploy0

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -3,7 +3,7 @@
 #$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'rvm/capistrano'
 set :rvm_ruby_string, '1.9.2'
-set :rvm_type, :user
+#set :rvm_type, :user
 
 # bundler bootstrap
 require 'bundler/capistrano'
