276da11db9a856081163e21a31924e32e867d791
Andrés Pereira <apereira@alabs.es>
2012-07-12 13:44:14 +0200

    no se comentan lineas en capistrano si no se comprueba q no se rompe nada ;)

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -1,7 +1,6 @@
 # RVM bootstrap
-# $:.unshift(File.expand_path("/home/tim/.rvm/lib"))
 #$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-#require 'rvm/capistrano'
+require 'rvm/capistrano'
 set :rvm_ruby_string, '1.9.2'
 set :rvm_type, :user
 
