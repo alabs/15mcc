ba60e4a00db70f29a5ce3a0eefc3ede25bf0e926
Antonio Pardo <apardo@alabs.es>
2012-09-27 17:51:35 +0200

    fix capistrano

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -1,7 +1,7 @@
 # RVM bootstrap
 #$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 #require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.2'
+set :rvm_ruby_string, '1.9.2@banco'
 #set :rvm_type, :user
 
 set :bundle_cmd, 'source $HOME/.bashrc && bundle'
